67753ca1cbc251af03aced4a6892243a5f4f7696
Kristian Kråkenes <Kristian.sk@hotmail.com>
2026-06-10 14:02:36 +0200

    Add settings modal and model for business settings management

---
+++ BlazingInvoices/Models/SettingsModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazingInvoices.Models;
+
+public class SettingsModel
+{
+    [Required, MaxLength(200)]
+    public string BuisnessName { get; set; }
+    
+    [Required, EmailAddress, MaxLength(200)]
+    public string Email { get; set; }
+    
+    [MaxLength(15)]
+    public string? ContactNumber { get; set; }
+    
+    [MaxLength(250)]
+    public string? Address { get; set; }
+}
